673c49d65e8783a3d1f33791c8f9611d11fc29bc
Pasindu Ravisara <121508486+PasinduRavisara@users.noreply.github.com>
2026-01-20 15:40:57 +0530

    Fix errors
    Co-Authored-By: Devdun Fernando <devfernando222@gmail.com>

--- Backend-Achieve+/src/main/java/com/achieveplusbe/repository/CommunityPostRepository.java
+++ Backend-Achieve+/src/main/java/com/achieveplusbe/repository/CommunityPostRepository.java
@@ -2,12 +2,11 @@ package com.achieveplusbe.repository;
 
 import com.achieveplusbe.model.CommunityPost;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @Repository
 public interface CommunityPostRepository extends JpaRepository<CommunityPost, Long> {
